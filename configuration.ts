024737442b6274b21bf1d763fb1d4fceac5c1762
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2024-04-19 09:16:30 -0400

    Staging (#2)
    * feat: add workflow permission automation
    
    * feat: change reviews to 0
    
    * feat: change reviews to null
    
    * feat: update to disable pr reviews
    
    ---------
    
    Signed-off-by: Samuel Voeller <12182390+xCykrix@users.noreply.github.com>

--- configuration.ts
+++ configuration.ts
@@ -28,11 +28,7 @@ export const upstream = {
     branch: 'main',
     required_status_checks: null,
     enforce_admins: true,
-    required_pull_request_reviews: {
-      dismiss_stale_reviews: true,
-      require_code_owner_reviews: true,
-      required_approving_review_count: null,
-    },
+    required_pull_request_reviews: null,
     restrictions: null,
     required_linear_history: true,
     allow_force_pushes: false,
