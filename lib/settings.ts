7114c70cbb1d95fd864bf719d234fb3182afd5b5
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2024-04-19 12:26:46 -0400

    Staging (#3)
    * feat: add workflow permission automation
    
    * feat: change reviews to 0
    
    * feat: change reviews to null
    
    * feat: update to disable pr reviews
    
    * feat: add auto secret push
    
    ---------
    
    Signed-off-by: Samuel Voeller <12182390+xCykrix@users.noreply.github.com>

--- lib/settings.ts
+++ lib/settings.ts
@@ -1,20 +1,27 @@
 import { Octokit } from 'npm:@octoherd/cli';
 import type { Repository } from 'npm:@octoherd/cli';
 import { upstream } from '../configuration.ts';
+import { encrypt } from './util/libsodium.ts';
 
 export async function settings(octokit: Octokit, repository: Repository): Promise<void> {
   try {
+    // Update Settings
     await octokit.request('PATCH /repos/{owner}/{repo}', {
       owner: repository.owner.login,
       repo: repository.name,
       ...upstream.repo,
     });
-    await octokit.request('PUT /repos/{owner}/{repo}/actions/permissions/workflow', {
+    
+    const req = await octokit.request('GET /repos/{owner}/{repo}/actions/secrets/public-key', {
       owner: repository.owner.login,
       repo: repository.name,
-      default_workflow_permissions: 'write' as const,
-      can_approve_pull_request_reviews: true,
-    })
+    });
+    await octokit.request('PUT /repos/{owner}/{repo}/actions/secrets/{secret_name}', {
+      owner: repository.owner.login,
+      repo: repository.name,
+      secret_name: 'PERSONAL_ACCESS_TOKEN',
+      encrypted_value: await encrypt(req.data.key, Deno.env.get('GH_ACCESS_TOKEN') ?? 'NULL')
+    });
   } catch (error: unknown) {
     if (error instanceof Error) {
       octokit.log.error(`Failed to write 'settings' to %s.`, repository.full_name);
