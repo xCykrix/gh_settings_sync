71eb3b16577bb019873d4d62ababf57224bac925
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2024-04-19 16:39:11 +0000

    feat: rebase to deno sodium

--- lib/util/libsodium.ts
+++ lib/util/libsodium.ts
@@ -1,12 +1,12 @@
-import * as sodium from 'npm:libsodium-wrappers';
+import sodium from "https://deno.land/x/sodium@0.2.0/basic.ts";
 
 export async function encrypt(key: string, value: string): Promise<string> {
   await sodium.ready
   // Convert Secret & Base64 key to Uint8Array.
-  const binkey = sodium.from_base64(key, sodium.base64_variants.ORIGINAL)
+  const binkey = sodium.from_base64(key)
   const binsec = sodium.from_string(value)
   // Encrypt the secret using LibSodium
   const encBytes = sodium.crypto_box_seal(binsec, binkey)
   // Convert encrypted Uint8Array to Base64
-  return sodium.to_base64(encBytes, sodium.base64_variants.ORIGINAL)
+  return sodium.to_base64(encBytes)
 }
